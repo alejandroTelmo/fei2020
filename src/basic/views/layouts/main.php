4c71860e3b19a9207968556fe9ee21b8a5649623
Alejandro Daniel Telmo <alejandrodanieltelmo@gmail.com>
2020-06-10 11:56:07 -0300

    methods y property computed-tutorial-6

--- src/basic/views/layouts/main.php
+++ src/basic/views/layouts/main.php
@@ -41,6 +41,7 @@ AppAsset::register($this);
             ['label' => 'Home', 'url' => ['/site/index']],
             ['label' => 'About', 'url' => ['/site/about']],
             ['label' => 'Contact', 'url' => ['/site/contact']],
+            ['label' => 'Vue', 'url' => ['/site/vue']],
             Yii::$app->user->isGuest ? (
                 ['label' => 'Login', 'url' => ['/site/login']]
             ) : (
--- src/basic/views/site/vue.php
+++ src/basic/views/site/vue.php
@@ -11,21 +11,113 @@ $this->params['breadcrumbs'][] = $this->title;
 $this->registerJsFile("https://cdn.jsdelivr.net/npm/vue/dist/vue.js",[
     'position'=>View::POS_HEAD
 ]);
-?>
-<div class="site-about">
-    <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-        This is the About vue.
-    </p>
+?>
+<div class="site-vue">
+
 <div id="app">
-  {{ message }}
+
+<button @click="mostrar = !mostrar">{{mostrar?'Ocultar':'Mostrar'}}</button> <p v-if="mostrar">Texto a ocultar</p>
 </div>
-<script>
+
+<div id="app1">
+<table class="table" id="productos">
+
+    <th scope="col" id="nombre">Nombre</th><th scope="col"id="descripcion">Descripción</th><th scope="col" id="precio">Precio</th><th scope="col" id="cantidad">Cantidad</th><th scope="col" id="agregar">Agregar</th>
+
+    <tr v-for="producto in productos"><td>{{producto.nombre}} </td><td>{{producto.descripcion}} </td><td>{{producto.precio}} </td><td><input  v-model="producto.cantidad" > </td> <td><button v-on:click="producto.cantidad++">+</button><button v-on:click="producto.cantidad--">-</button></td></tr>
+
+      <input v:model="nuevoNombre" placeholder="nombre" >
+
+      <input v:model="nuevaDescripcion" placeholder="descripcion" >
+
+      <input v:model.number="nuevoPrecio" placeholder="precio" >
+
+      <button v-on:click="agregarProducto">+</button>
+</table>
+<span>El total de todos los precios es : {{totalProductos}} </span><hr>
+     
+  <input v-model="napellido" placeholder="ingrese su apellido"><p>Hola {{mio}}, esperamos tu consulta ! </p>
+  <input v-model.number="nedad" placeholder="ingrese su edad"><p v-if="edad > 20 && edad < 30">Estas hecho un pibe {{mio}}, esperamos tu consulta ! </p>
+  <button @click="mas">+</button>
+  <ul v-for="cliente in clientes">
+  <li> Hola {{cliente.apellido}}</li><span>Edad : {{cliente.edad}} </span><span>Corregir edad </span><span> <button v-on:click="cliente.edad++">+</button><button v-on:click="cliente.edad--">-</button> </span>
+
+  </ul>
+  La edad total de los clientes es : {{totalEdad}}
+</div>
+
+
+<script> 
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hola Vue!'
+  
+    hint:'texto a ocultar',
+    mostrar:false,
+   
+  },
+})
+
+
+
+var app1 = new Vue({
+  el: '#app1',
+  data: {
+    nuevoNombre:'',
+    nuevaDescripcion:'',
+    nuevoPrecio:0,
+    cantidad:0,
+    mio:'',
+    nedad:0,
+    napellido:'',
+    clientes:[
+            {apellido:"telmo",edad:42},
+            {apellido:"sepulveda",edad:39}
+    ],
+    productos:[
+      {nombre:'Medias', descripcion:'Medias 3/4',precio:120 ,cantidad:0},
+      {nombre:'Pantalon', descripcion:'Jean elastizado',precio:135,cantidad:0},
+      {nombre:'Camisa', descripcion:'Cuadrille',precio:155,cantidad:0 },
+      {nombre:'Remera', descripcion:'M/larga',precio:189 ,cantidad:0}
+    ]
+  }, methods: {
+    agregarProducto(){
+      this.productos.push(
+        {
+        nombre: this.nuevoNombre,
+        descripcion: this.nuevaDescripcion,
+        precio: this.nuevoPrecio,
+        cantidad:0
+        }
+      )
+  },mas(){
+    this.clientes.push(
+      {
+        apellido:this.napellido,
+      edad:this.nedad
+      }
+      
+    )
+    this.napellido='',
+    this.nedad=0
+  }
+  }
+,computed:{
+    totalEdad(){
+      total=0
+      for(cliente of this.clientes){
+        total+=cliente.edad;
+      }
+      return total;
+    },
+    totalProductos(){
+      totales=0
+      for(producto of this.productos){
+        totales+=producto.precio;
+      }
+      return totales;
+    }
   }
 })
 </script>
